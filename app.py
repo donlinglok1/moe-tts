f60cca26b45066aa6c949405cceefca3b096604c
skytnt <SkyTNT@outlook.com>
2022-08-29 14:52:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import os
 import librosa
 import numpy as np
 import torch
@@ -7,14 +8,14 @@ import commons
 import utils
 import gradio as gr
 from models import SynthesizerTrn
-from text import text_to_sequence
+from text import text_to_sequence, _clean_text
 from mel_processing import spectrogram_torch
 
-limitation = True  # limit text and audio length
+limitation = os.getenv("SYSTEM") == "spaces"  # limit text and audio length in huggingface spaces
 
 
-def get_text(text, hps):
-    text_norm = text_to_sequence(text, hps.symbols, hps.data.text_cleaners)
+def get_text(text, hps, is_phoneme):
+    text_norm = text_to_sequence(text, hps.symbols, [] if is_phoneme else hps.data.text_cleaners)
     if hps.data.add_blank:
         text_norm = commons.intersperse(text_norm, 0)
     text_norm = LongTensor(text_norm)
@@ -22,11 +23,11 @@ def get_text(text, hps):
 
 
 def create_tts_fn(model, hps, speaker_ids):
-    def tts_fn(text, speaker, speed):
-        if limitation and len(text) > 60:
+    def tts_fn(text, speaker, speed, is_phoneme):
+        if limitation and ((len(text) > 60 and not is_phoneme) or (len(text) > 120 and is_phoneme)):
             return "Error: Text is too long", None
         speaker_id = speaker_ids[speaker]
-        stn_tst = get_text(text, hps)
+        stn_tst = get_text(text, hps, is_phoneme)
         with no_grad():
             x_tst = stn_tst.unsqueeze(0)
             x_tst_lengths = LongTensor([stn_tst.size(0)])
@@ -72,6 +73,24 @@ def create_vc_fn(model, hps, speaker_ids):
     return vc_fn
 
 
+css = """
+        #advanced-btn {
+            color: white;
+            border-color: black;
+            background: black;
+            font-size: .7rem !important;
+            line-height: 19px;
+            margin-top: 24px;
+            margin-bottom: 12px;
+            padding: 2px 8px;
+            border-radius: 14px !important;
+        }
+        #advanced-options {
+            display: none;
+            margin-bottom: 20px;
+        }
+"""
+
 if __name__ == '__main__':
     models = []
     with open("saved_model/names.json", "r", encoding="utf-8") as f:
@@ -92,10 +111,10 @@ if __name__ == '__main__':
         speaker_ids = [sid for sid, name in enumerate(hps.speakers) if name != "None"]
         speakers = [name for sid, name in enumerate(hps.speakers) if name != "None"]
 
-        models.append((models_name, cover_path, speakers,
+        models.append((models_name, cover_path, speakers, hps.symbols,
                        create_tts_fn(model, hps, speaker_ids), create_vc_fn(model, hps, speaker_ids)))
 
-    app = gr.Blocks()
+    app = gr.Blocks(css=css)
 
     with app:
         gr.Markdown("# Moe Japanese TTS And Voice Conversion Using VITS Model\n\n"
@@ -107,7 +126,7 @@ if __name__ == '__main__':
         with gr.Tabs():
             with gr.TabItem("TTS"):
                 with gr.Tabs():
-                    for i, (model_name, cover_path, speakers, tts_fn, vc_fn) in enumerate(models):
+                    for i, (model_name, cover_path, speakers, symbols, tts_fn, vc_fn) in enumerate(models):
                         with gr.TabItem(f"model{i}"):
                             with gr.Column():
                                 gr.Markdown(f"## {model_name}\n\n"
@@ -116,14 +135,31 @@ if __name__ == '__main__':
                                 tts_input2 = gr.Dropdown(label="Speaker", choices=speakers,
                                                          type="index", value=speakers[0])
                                 tts_input3 = gr.Slider(label="Speed", value=1, minimum=0.5, maximum=2, step=0.1)
+                                advanced_button = gr.Button("Advanced options", elem_id="advanced-btn")
+                                advanced_options = gr.Column()
+                                advanced_options.elem_id = "advanced-options"
+                                with advanced_options:
+                                    phoneme_input = gr.Checkbox(value=False, label="Phoneme input")
+                                    to_phoneme_btn = gr.Button("Covert text to phoneme")
+                                    phoneme_list = gr.Json(label="Phoneme list", value=symbols, elem_id="phoneme_list")
+
                                 tts_submit = gr.Button("Generate", variant="primary")
                                 tts_output1 = gr.Textbox(label="Output Message")
                                 tts_output2 = gr.Audio(label="Output Audio")
-                                tts_submit.click(tts_fn, [tts_input1, tts_input2, tts_input3],
+                                advanced_button.click(None, [], [],
+                                                      _js="""
+                                                        () => {
+                                                            const options = document.querySelector("body > gradio-app").shadowRoot.querySelector("#advanced-options");
+                                                            options.style.display = ["none", ""].includes(options.style.display) ? "flex" : "none";
+                                                        }""")
+                                tts_submit.click(tts_fn, [tts_input1, tts_input2, tts_input3, phoneme_input],
                                                  [tts_output1, tts_output2])
+                                to_phoneme_btn.click(lambda x: _clean_text(x, hps.data.text_cleaners) if x != "" else x,
+                                                     [tts_input1], [tts_input1])
+
             with gr.TabItem("Voice Conversion"):
                 with gr.Tabs():
-                    for i, (model_name, cover_path, speakers, tts_fn, vc_fn) in enumerate(models):
+                    for i, (model_name, cover_path, speakers, symbols, tts_fn, vc_fn) in enumerate(models):
                         with gr.TabItem(f"model{i}"):
                             gr.Markdown(f"## {model_name}\n\n"
                                         f"![cover](file/{cover_path})")
@@ -136,4 +172,4 @@ if __name__ == '__main__':
                             vc_output1 = gr.Textbox(label="Output Message")
                             vc_output2 = gr.Audio(label="Output Audio")
                             vc_submit.click(vc_fn, [vc_input1, vc_input2, vc_input3], [vc_output1, vc_output2])
-    app.launch(max_threads=10)
+    app.launch()
