58493c0f166ba4fd6087609f5c2b67e0431906b0
skytnt <SkyTNT@outlook.com>
2022-09-12 21:05:20 +0800

    add soft-vits and more models

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ emoji: 😊🎙️
 colorFrom: red
 colorTo: pink
 sdk: gradio
-sdk_version: 3.2
+sdk_version: 3.3
 app_file: app.py
 pinned: false
 license: mit
--- app.py
+++ app.py
@@ -84,6 +84,34 @@ def create_vc_fn(model, hps, speaker_ids):
     return vc_fn
 
 
+def create_soft_vc_fn(model, hps, speaker_ids):
+    def soft_vc_fn(target_speaker, input_audio):
+        if input_audio is None:
+            return "You need to upload an audio", None
+        sampling_rate, audio = input_audio
+        duration = audio.shape[0] / sampling_rate
+        if limitation and duration > 15:
+            return "Error: Audio is too long", None
+        target_speaker_id = speaker_ids[target_speaker]
+
+        audio = (audio / np.iinfo(audio.dtype).max).astype(np.float32)
+        if len(audio.shape) > 1:
+            audio = librosa.to_mono(audio.transpose(1, 0))
+        if sampling_rate != 16000:
+            audio = librosa.resample(audio, orig_sr=sampling_rate, target_sr=16000)
+        with torch.inference_mode():
+            units = hubert.units(torch.FloatTensor(audio).unsqueeze(0).unsqueeze(0))
+        with no_grad():
+            unit_lengths = LongTensor([units.size(1)])
+            sid = LongTensor([target_speaker_id])
+            audio = model.infer(units, unit_lengths, sid=sid, noise_scale=.667,
+                                noise_scale_w=0.8)[0][0, 0].data.cpu().float().numpy()
+        del units, unit_lengths, sid
+        return "Success", (hps.data.sampling_rate, audio)
+
+    return soft_vc_fn
+
+
 def create_to_phoneme_fn(hps):
     def to_phoneme_fn(text):
         return _clean_text(text, hps.data.text_cleaners) if text != "" else ""
@@ -110,7 +138,9 @@ css = """
 """
 
 if __name__ == '__main__':
-    models = []
+    models_tts = []
+    models_vc = []
+    models_soft_vc = []
     with open("saved_model/info.json", "r", encoding="utf-8") as f:
         models_info = json.load(f)
     for i, info in models_info.items():
@@ -132,9 +162,16 @@ if __name__ == '__main__':
         speaker_ids = [sid for sid, name in enumerate(hps.speakers) if name != "None"]
         speakers = [name for sid, name in enumerate(hps.speakers) if name != "None"]
 
-        models.append((name, lang, example, cover_path, speakers, hps.symbols,
-                       create_tts_fn(model, hps, speaker_ids), create_vc_fn(model, hps, speaker_ids),
-                       create_to_phoneme_fn(hps)))
+        t = info["type"]
+        if t == "vits":
+            models_tts.append((name, cover_path, speakers, lang, example,
+                               hps.symbols, create_tts_fn(model, hps, speaker_ids),
+                               create_to_phoneme_fn(hps)))
+            models_vc.append((name, cover_path, speakers, create_vc_fn(model, hps, speaker_ids)))
+        elif t == "soft-vits-vc":
+            models_soft_vc.append((name, cover_path, speakers, create_soft_vc_fn(model, hps, speaker_ids)))
+
+    hubert = torch.hub.load("bshall/hubert:main", "hubert_soft")
 
     app = gr.Blocks(css=css)
 
@@ -144,13 +181,14 @@ if __name__ == '__main__':
                     "unofficial demo for \n\n"
                     "- [https://github.com/CjangCjengh/MoeGoe](https://github.com/CjangCjengh/MoeGoe)\n"
                     "- [https://github.com/Francis-Komizu/VITS](https://github.com/Francis-Komizu/VITS)\n"
-                    "- [https://github.com/luoyily/MoeTTS](https://github.com/luoyily/MoeTTS)"
+                    "- [https://github.com/luoyily/MoeTTS](https://github.com/luoyily/MoeTTS)\n"
+                    "- [https://github.com/Francis-Komizu/Sovits](https://github.com/Francis-Komizu/Sovits)"
                     )
         with gr.Tabs():
             with gr.TabItem("TTS"):
                 with gr.Tabs():
-                    for i, (name, lang, example, cover_path, speakers,
-                            symbols, tts_fn, vc_fn, to_phoneme_fn) in enumerate(models):
+                    for i, (name, cover_path, speakers, lang, example, symbols, tts_fn,
+                            to_phoneme_fn) in enumerate(models_tts):
                         with gr.TabItem(f"model{i}"):
                             with gr.Column():
                                 gr.Markdown(f"## {name}\n\n"
@@ -204,8 +242,7 @@ if __name__ == '__main__':
 
             with gr.TabItem("Voice Conversion"):
                 with gr.Tabs():
-                    for i, (name, lang, example, cover_path, speakers,
-                            symbols, tts_fn, vc_fn, to_phoneme_fn) in enumerate(models):
+                    for i, (name, cover_path, speakers, vc_fn) in enumerate(models_vc):
                         with gr.TabItem(f"model{i}"):
                             gr.Markdown(f"## {name}\n\n"
                                         f"![cover](file/{cover_path})")
@@ -218,4 +255,17 @@ if __name__ == '__main__':
                             vc_output1 = gr.Textbox(label="Output Message")
                             vc_output2 = gr.Audio(label="Output Audio")
                             vc_submit.click(vc_fn, [vc_input1, vc_input2, vc_input3], [vc_output1, vc_output2])
+            with gr.TabItem("Soft Voice Conversion"):
+                with gr.Tabs():
+                    for i, (name, cover_path, speakers,soft_vc_fn) in enumerate(models_soft_vc):
+                        with gr.TabItem(f"model{i}"):
+                            gr.Markdown(f"## {name}\n\n"
+                                        f"![cover](file/{cover_path})")
+                            vc_input1 = gr.Dropdown(label="Target Speaker", choices=speakers, type="index",
+                                                    value=speakers[0])
+                            vc_input2 = gr.Audio(label="Input Audio (15s limitation)")
+                            vc_submit = gr.Button("Convert", variant="primary")
+                            vc_output1 = gr.Textbox(label="Output Message")
+                            vc_output2 = gr.Audio(label="Output Audio")
+                            vc_submit.click(soft_vc_fn, [vc_input1, vc_input2], [vc_output1, vc_output2])
     app.launch()
